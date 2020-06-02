8558d1e45489f2bea5d055d109fe530a96965f78
sharkwarn <sharkwarn@163.com>
2020-06-03 00:29:22 +0800

    添加start.sh

--- _book/src/index.js
+++ _book/src/index.js
@@ -1,9 +1,43 @@
-var obj = {
-    a:1,
-    foo: function() {
-        console.log(this.a);
+
+const obj = {
+    a: 1,
+    b: null,
+    c: {
+        d: 1,
+        e: {
+            f: 3
+        }
+    },
+    h: [{a: 1}, {b: 2}, 3, 4, 5]
+};
+
+obj.c.e.g = obj.c;
+
+
+function clone(target) {
+    const result = {};
+    const container = Object.create(null);
+    container[result] = result;
+    function deepClone(current, result, container) {
+        for (let key in current) {
+            if (typeof current[key] === Object) {
+                if (container[current[key]]) {
+                    result[key] = container[current[key]];
+                } else {
+                    result[key] = {};
+                    if (Array.isArray(current[key])) {
+                        result[key] = [];
+                    }
+                    deepClone(current[key], result, container);
+                }
+            } else {
+                result[key] = current[key];
+            }
+        }
     }
+    deepClone(target, result, container);
+    return result;
 }
 
-var bar = obj.foo;
-bar();
+const res = clone(obj);
+console.log(res, 2222);
